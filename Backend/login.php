b59b1c39ca08d4f41c1a56f4ba46a5fc1851a0a8
abanob96 <Abanobmikaeel@gmail.com>
2018-03-18 18:42:59 -0400

    Fixing backend

--- Backend/login.php
+++ Backend/login.php
@@ -1,16 +1,12 @@
 <?php
 session_start();
-ini_set("display_errors", 1);
-ini_set("log_errors",1);
-ini_set("error_log", "/tmp/error.log");
-error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
-error_log("Hello, errors!");
+error_reporting(0); // Disable all errors.
 
 include ('client.php');
 
 $user = $_POST['user'];
 $pass = $_POST['pass'];
-$response = authentication($user, $pass);
+// $response = authentication($user, $pass);
 
 /*
 if($response == false)  {
@@ -24,6 +20,6 @@ else {
 }
 */
 
-return true;
+echo($user +" "+ $pass);
 
 ?>
--- Game/New Unity Project/Assets/Scripts/LoginPage.cs
+++ Game/New Unity Project/Assets/Scripts/LoginPage.cs
@@ -10,7 +10,8 @@ public class LoginPage : MonoBehaviour {
 	public InputField UsernameField;
 	public InputField PasswordField;
 	bool loginCorrect;
-
+	private static readonly string POSTAddUserURL = "http://afsaccess1.njit.edu/~sg873/login.html";
+	
 	//This scripts add a listener to a the panel and loads the menu scene user the fade script
 	void Start () {
 		LoginButton.GetComponent<Button> ().onClick.AddListener (() => Login(UsernameField.text, PasswordField.text));
@@ -21,6 +22,7 @@ public class LoginPage : MonoBehaviour {
 		loginCorrect = true;
 		print (username);
 		print (password);
+		loginCorrect = SendLogin (username, password);
 
 		if (loginCorrect) {
 			Load ("menu");
@@ -29,6 +31,35 @@ public class LoginPage : MonoBehaviour {
 			print("error can't login");
 		}
 	}
+
+	// Send Login 
+	public bool SendLogin(string username, string password) {
+		WWW www;
+		Hashtable postHeader = new Hashtable();
+		postHeader.Add("Content-Type", "application/json");
+		WWWForm form = new WWWForm();
+
+		string json = @"{ 'username':'"+username+"','password':'"+password+"'}";
+
+		print(json);
+		form.AddField("data", json);
+		www = new WWW(POSTAddUserURL, form);
+		StartCoroutine(WaitForRequest(www));
+
+		return false;
+	}
+
+	// Callback implmentation
+	IEnumerator WaitForRequest(WWW data) {
+		yield return data; // Wait until the download is done
+		if (data.error != null) {
+			print("There was an error sending request: " + data.error);
+		}
+		else {
+			print("WWW Request: " + data.text);
+		}
+	}
+
 
 	public void Load (string sceneName) {
 		Initiate.Fade(sceneName, Color.white, 1.0f);
